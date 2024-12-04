baf0150db946e9bebc82a69e461c8984dedb5384
Rogier Lankhorst <rogierlankhorst@gmail.com>
2024-12-04 12:36:59 +0100

    build

--- includes/Admin/App/build/index.asset.php
+++ includes/Admin/App/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'cb485f6a63c4b8e673b3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '86ce0c2fed11d51907ad');
