6a60d609dffbf587a661019f36f60d2915867f18
Rogier Lankhorst <rogierlankhorst@gmail.com>
2025-02-13 09:37:13 +0100

    palette loading preview

--- includes/Admin/App/build/index.asset.php
+++ includes/Admin/App/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f8ed72dd9827f748aec4');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '137472f43c5276dfe546');
