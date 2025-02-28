63c9d750697122e691482e018faff99c75fee4f7
willembraam <willem.braam@team.blue>
2025-02-28 16:27:27 +0100

    nfc: todo

--- config/features.php
+++ config/features.php
@@ -4,32 +4,12 @@
 
 return [
     'Onboarding' => [
-        'enabled' => true, // todo - we need some way to track if the onboarding is completed
+        'enabled' => true, // todo - we need some way to track if the onboarding is completed and disable this feature if it is
         'inScope' => is_admin(),
         'pro' => false,
         'dependencies' => [
             'Service',
             '\Simplybook_old\Api\Api',
-        ]
-    ],
-    'ExampleFeature' => [ // Folder- and feature name. Prefix all classes with the feature name.
-        // 'enabled' => get_option('example_feature_enabled'), // default false
-        'enabled' => true, // for testing purposes
-        'inScope' => is_admin(), // dynamic check to see if feature should be loaded, default true
-        'pro' => false, // default false
-        'dependencies' => [ // Controller dependencies, optional.
-            'Service', // Will be instantiated as ExampleFeatureService from feature folder
-            'Repository',
-            // '\ExampleRoot\Namespace\Class', // Fully qualified class name will be instantiated from root
-        ]
-    ],
-    'ExampleProFeature' => [
-        'enabled' => true,
-        'inScope' => is_admin(),
-        'pro' => true,
-        'dependencies' => [
-            'Service',
-            'Repository',
         ]
     ],
 ];
