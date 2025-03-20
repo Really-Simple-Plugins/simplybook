3cc8484c42d0cf62aba387cdbed945f4dd7a7692
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-03-20 14:17:08 +0100

    Feature: Build created

--- react/build/index.asset.php
+++ react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1f1c39cf36b074a40f3e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '97b20c72fd199cf4b228');
