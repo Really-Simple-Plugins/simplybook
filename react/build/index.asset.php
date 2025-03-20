e5731b9a2b1e9eb62ac78dad3326394c3864cf21
willembraam <willem.braam@team.blue>
2025-03-20 15:15:09 +0100

    fix: we can now write tailwind without importanting everything.

--- react/build/index.asset.php
+++ react/build/index.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2372d5f731b29fd85288');
+=======
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '97b20c72fd199cf4b228');
+>>>>>>> 2ec4b77 (fix: we can now write tailwind without importanting everything.)
