4712aca9e9323af4a3ff3f95d8f19d21995ba544
willembraam <willem.braam@team.blue>
2025-03-24 13:32:48 +0100

    feat: added SubscriptionData.ts as a typescript model
    It fetches the subscription data from our rest API and can be used to render data in our dashboard,

---
+++ react/src/types/SubscriptionData.ts
@@ -0,0 +1,21 @@
+export interface SubscriptionData {
+    data: {
+        subscription_name: string;
+        expires_in: string;
+        is_expired: boolean;
+        limits: {
+            sms_limits: {
+                rest: number;
+                total: number;
+            };
+            sheduler_limit: { // Typo intended, its misspelled in the API
+                rest: number;
+                total: number;
+            };
+            provider_limit: {
+                rest: number;
+                total: number;
+            };
+        };
+    }
+}
