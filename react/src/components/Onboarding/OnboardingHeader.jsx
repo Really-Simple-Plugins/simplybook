ec30c114e1f9b642afc48b6e1984af9f171a6628
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-03-26 13:00:23 +0100

    Fix: Changed header and container padding and added function comment

--- react/src/components/Onboarding/OnboardingHeader.jsx
+++ react/src/components/Onboarding/OnboardingHeader.jsx
@@ -2,34 +2,39 @@ import React, { useState } from "react";
 import clsx from "clsx";
 import {__} from "@wordpress/i18n";
 import { ReactComponent as Logo } from "../../../../assets/img/logo.svg";
-import SignInModal from "../Modals/SignInModal.js";
+import SignInModal from "../Modals/SignInModal";
 
 const OnboardingHeader = ({
     className, 
     signInLink 
 }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-  
+    
+    /**
+     * Toggle the SignInModal
+     * 
+     * @param {e} e 
+     */
     const toggleModal = (e) => {
         e.preventDefault();
         setIsModalOpen(!isModalOpen);
     };
 
     return (
-        <div className={clsx(className, "flex w-full items-center justify-between")}>
+        <header className={clsx(className, "flex w-full items-center justify-between")}>
             {isModalOpen && <SignInModal onClose={toggleModal}/>}
             <div className="flex flex-row justify-between w-full px-4">
-                <Logo className="mx-4 w-40 py-8" />
+                <Logo className="w-40 py-8" />
                 <div className="flex items-center">
                     <span className={"m-5 text-black"}>
-                        {__("Already got an account?", "simplybook")}{" "}
+                        {__("Already got an account?", "simplybook")}
                     </span>
                     <a className="font-bold text-primary" href="#" onClick={toggleModal}>
                         {__("Sign in here")}
-                    </a>{" "}
+                    </a>
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
 
