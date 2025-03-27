be5b207be642e470c4fcf6b2676cfa37c3395cf0
willembraam <willem.braam@team.blue>
2025-03-27 11:30:34 +0100

    build: commit contains build files for next commit

--- react/build/index.asset.php
+++ react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c77bd81889c1dd069583');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2372d5f731b29fd85288');
