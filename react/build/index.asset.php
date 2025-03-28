6365677cf28cdf28ce0fd5a6e378b4f3600663bb
willembraam <willem.braam@team.blue>
2025-03-28 16:48:24 +0100

    refactor: doPluginAction.tsx is now deprecated, relevant hooks now use HttpClient

--- react/build/index.asset.php
+++ react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '543cc9d18c236d001b48');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '13a524dc475919831af8');
--- react/src/hooks/useOtherPluginsData.tsx
+++ react/src/hooks/useOtherPluginsData.tsx
@@ -2,23 +2,25 @@ import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import {OtherPlugins} from "../types/OtherPlugins";
 import {OtherPlugin} from "../types/OtherPlugin";
 import {__} from "@wordpress/i18n";
-import doPluginAction from "../api/endpoints/Dashboard/doPluginAction";
 import React from "react";
 import HttpClient from "../api/requests/HttpClient";
 
 const useOtherPluginsData = () => {
 
-    const route = 'other_plugins_data';
-    const client = new HttpClient(route);
+    const pluginActionRoute = 'do_plugin_action';
+    const otherPluginsDataRoute = 'other_plugins_data';
+    const client = new HttpClient();
 
     // Query for fetching settings from server
     const {isLoading, error, data: response} = useQuery({
-        queryKey: [route],
-        queryFn: () => client.get(),
+        queryKey: [otherPluginsDataRoute],
+        queryFn: () => client.setRoute(otherPluginsDataRoute).get(),
         staleTime: 1000 * 60 * 60,
         retry: 0,
         enabled: true,
     });
+
+    const relatedReallySimplePlugins: OtherPlugins = response?.data?.plugins;
 
     if (error !== null) {
         console.error('Error fetching related plugins: ', error.message);
@@ -26,22 +28,28 @@ const useOtherPluginsData = () => {
 
     const queryClient = useQueryClient();
     const updatePluginData = (slug: string, newPluginItem: OtherPlugin) => {
-        if (!response) {
+        if (!relatedReallySimplePlugins) {
             return;
         }
 
-        queryClient.setQueryData([route], (oldData: OtherPlugins) => {
-            return oldData.map((plugin: OtherPlugin) =>
-                plugin.slug === slug ? newPluginItem : plugin,
+        queryClient.setQueryData([otherPluginsDataRoute], (oldResponse: any) => {
+
+            oldResponse.data.plugins = Object.fromEntries(
+                Object.entries(oldResponse?.data?.plugins as Record<string, OtherPlugin>).map(
+                    ([key, plugin]: [string, OtherPlugin]) =>
+                        plugin.slug === slug ? [key, newPluginItem] : [key, plugin]
+                )
             );
+
+            return oldResponse;
         });
     };
 
     const getPluginData = (slug: string) => {
-        if (!response) {
+        if (!relatedReallySimplePlugins) {
             return;
         }
-        return response?.data?.plugins.find((plugin: OtherPlugin) => plugin.slug === slug);
+        return Object.values(relatedReallySimplePlugins).find((plugin: OtherPlugin) => plugin.slug === slug);
     };
 
     const pluginActionNice = (action: string) => {
@@ -57,24 +65,24 @@ const useOtherPluginsData = () => {
     };
 
     const runPluginAction = useMutation({
-        mutationFn: async ({
-           slug,
-           action,
-           e,
-       }: {
+        mutationFn: async ({slug, action, e,}: {
             slug: string;
             action: string;
             e?: React.MouseEvent<HTMLAnchorElement, MouseEvent>;
         }) => {
+
             if (action === "installed" || action === "upgrade-to-premium") {
                 return;
             }
 
             if (e) e.preventDefault();
+
             let data: any = {};
             data.slug = slug;
             data.action = action;
+
             let pluginItem = getPluginData(slug);
+
             if (!pluginItem) return;
 
             if (action === "download") {
@@ -82,26 +90,47 @@ const useOtherPluginsData = () => {
             } else if (action === "activate") {
                 pluginItem.action = "activating";
             }
+
             updatePluginData(slug, pluginItem);
 
             if (action === "installed" || action === "upgrade-to-premium") {
                 return;
             }
 
-            let updatedPluginItem = await doPluginAction(slug, action);
-            console.log("resulting plugin item", updatedPluginItem);
+            // let updatedPluginItem = await doPluginAction(slug, action);
+            let updatedPluginItemResponse = await client.setRoute(pluginActionRoute).setPayload({
+                'slug': slug,
+                'action': action,
+            }).post();
+
+            let updatedPluginItem = updatedPluginItemResponse?.data?.plugin;
+            if (!updatedPluginItem) {
+                console.error('Error fetching updated plugin item: ', updatedPluginItemResponse?.message);
+                return;
+            }
+
             //if the plugin was downloaded, we now activate.
             if (updatedPluginItem.action === "activate") {
                 pluginItem.action = "activating";
                 updatePluginData(slug, pluginItem);
-                updatedPluginItem = await doPluginAction(slug, "activate");
+                // updatedPluginItem = await doPluginAction(slug, "activate");
+                updatedPluginItemResponse = await client.setRoute(pluginActionRoute).setPayload({
+                    'slug': slug,
+                    'action': 'activate',
+                }).post();
+
+                updatedPluginItem = updatedPluginItemResponse?.data?.plugin;
+                if (!updatedPluginItem) {
+                    console.error('Error 2 fetching updated plugin item: ', updatedPluginItemResponse?.message);
+                    return;
+                }
             }
             updatePluginData(slug, updatedPluginItem);
         },
     });
 
     return {
-        plugins: response?.data?.plugins,
+        plugins: relatedReallySimplePlugins,
         fetched: !isLoading,
         pluginActionNice,
         runPluginAction: runPluginAction.mutate,
