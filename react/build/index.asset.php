0fb96e08eba21675ff0046d2ee397beb70d564e0
willembraam <willem.braam@team.blue>
2025-03-28 15:08:48 +0100

    build: it's a build!

--- react/build/index.asset.php
+++ react/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '101edb8db52962cc279b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '780cab305ad708bfb2c8');
