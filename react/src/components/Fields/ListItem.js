2a8ef17cdee1c61ef98cdc5de8e49d37013d9830
willembraam <willem.braam@team.blue>
2025-03-28 11:43:29 +0100

    refactor: Better handling loading, error and success states in ListItem.js for domain fetching
    This is doable as useDomainData is now using HttpClient

--- react/src/components/Fields/ListItem.js
+++ react/src/components/Fields/ListItem.js
@@ -17,7 +17,7 @@ const ListItem = forwardRef(
             console.log('onChange', e.target.checked, "for id ", item.id);
             setVisible(e.target.checked);
         };
-        const { domain, domainFetched } = useDomainData();
+        const { domain, domainFetched, hasError: domainHasError } = useDomainData();
         const hasPicture = domainFetched && item.picture_preview && item.picture_preview.length > 0;
         const fullLabel = upgrade? ' |  '+sprintf(__("Get unlimited %s", "simplybook"), label) : __("Edit", "simplybook");
 
@@ -26,25 +26,26 @@ const ListItem = forwardRef(
                 <div className="w-full flex items-center justify-between px-4 py-5 bg-gray-100 mb-4">
                     <div className="flex items-center space-x-3">
                         <div>
-                            { hasPicture &&
-                                <img className="w-15 h-15 max-w-[40px] max-h-[40px]" src={domain + item.picture_preview}
-                                     alt={item.picture_preview} />
+                            {domainFetched && !domainHasError && hasPicture &&
+                                <img className="w-15 h-15 max-w-[40px] max-h-[40px]" src={domain + item.picture_preview}  alt={__('Loading', 'simplybook')}/>
                             }
-                            {!hasPicture &&
+                            {(!domainFetched || domainHasError || !hasPicture) &&
                                 <div className="min-w-[40px] min-h-[40px] flex items-center justify-center">
                                     <Icon name="cart"/>
                                 </div>
                             }
                         </div>
                         <div className="font-bold">{item.name}</div>
-                        <LoginLink page={link}>
-                            {fullLabel}
-                        </LoginLink>
+                        {domainFetched && !domainHasError &&
+                            <LoginLink page={link}>
+                                {fullLabel}
+                            </LoginLink>
+                        }
                     </div>
                     <div className="flex items-center">
 
                         <div className="ml-4 relative">
-                            {!upgrade &&
+                            {domainFetched && !domainHasError && !upgrade &&
                                 <CheckboxInput
                                     label={""}
                                     id={item.id}
@@ -53,10 +54,10 @@ const ListItem = forwardRef(
                                 />
                             }
                             {upgrade && <>
-                                            <span
-                                                className={`inline-block w-[100px] text-center px-3 py-1.5 rounded-md text-xs font-medium bg-tertiary text-white`}>
-                                              {__("Upgrade", "simplybook")}
-                                            </span>
+                                <span
+                                    className={`inline-block w-[100px] text-center px-3 py-1.5 rounded-md text-xs font-medium bg-tertiary text-white`}>
+                                  {__("Upgrade", "simplybook")}
+                                </span>
                             </>}
                         </div>
                     </div>
