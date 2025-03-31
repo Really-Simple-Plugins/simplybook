2fa383bf40f31f349f6e91491086bbfba7552275
willembraam <willem.braam@team.blue>
2025-03-31 08:52:57 +0200

    style: re-indent with 4 spaces

--- react/src/hooks/useSettingsData.js
+++ react/src/hooks/useSettingsData.js
@@ -9,65 +9,65 @@ import saveSettingsFields from "../api/endpoints/saveSettings";
  * @returns {Object} - An object containing settings data, update function, and status flags.
  */
 const useSettingsData = () => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const transformData = (data) => {
-    //find all items with type===checkbox, and ensure that the value is a boolean
-    return data.map((field) => {
-      if (field.type === "checkbox") {
-        field.value = !!field.value;
-      }
-      return field;
-    });
-  };
-
-  const query = useQuery({
-    queryKey: ["settings_fields"],
-    queryFn: () => getSettingsFields({ withValues: true }),
-    staleTime: 1000 * 60 * 5, // 5 minutes
-    initialData: transformData(
-      window.simplybook && window.simplybook.settings_fields,
-    ),
-    retry: 0,
-    select: (data) => (data ? [...data] : []),
-  });
-
-  const getValue = (id) => {
-    return query.data.find((field) => field.id === id)?.value;
-  };
-  const setValue = (id, value) => {
-    queryClient.setQueryData(["settings_fields"], (oldData) => {
-      return oldData.map((field) =>
-        field.id === id ? { ...field, value } : field,
-      );
-    });
-  };
-
-  // Update Mutation for settings data with destructured values
-  const { mutateAsync: saveSettings, isLoading: isSavingSettings } =
-    useMutation({
-      mutationFn: async (data) => {
-        console.log("saving data", data);
-        let settings = await saveSettingsFields(data);
-        return transformData(settings);
-      },
-      onSuccess: async (data) => {
-        queryClient.setQueryData(["settings_fields"], (oldData) => {
-          return data ? [...data] : [];
+    const transformData = (data) => {
+        //find all items with type===checkbox, and ensure that the value is a boolean
+        return data.map((field) => {
+            if (field.type === "checkbox") {
+                field.value = !!field.value;
+            }
+            return field;
         });
-        queryClient.invalidateQueries(["settings_fields"]);
-      },
+    };
+
+    const query = useQuery({
+        queryKey: ["settings_fields"],
+        queryFn: () => getSettingsFields({ withValues: true }),
+        staleTime: 1000 * 60 * 5, // 5 minutes
+        initialData: transformData(
+            window.simplybook && window.simplybook.settings_fields,
+        ),
+        retry: 0,
+        select: (data) => (data ? [...data] : []),
     });
 
-  return {
-    settings: query.data,
-    saveSettings,
-    getValue,
-    setValue,
-    isSavingSettings: query.isLoading || query.fetchStatus === "fetching",
-    invalidateSettings: () =>
-      queryClient.invalidateQueries(["settings_fields"]),
-  };
+    const getValue = (id) => {
+        return query.data.find((field) => field.id === id)?.value;
+    };
+    const setValue = (id, value) => {
+        queryClient.setQueryData(["settings_fields"], (oldData) => {
+            return oldData.map((field) =>
+                field.id === id ? { ...field, value } : field,
+            );
+        });
+    };
+
+    // Update Mutation for settings data with destructured values
+    const { mutateAsync: saveSettings, isLoading: isSavingSettings } =
+        useMutation({
+            mutationFn: async (data) => {
+                console.log("saving data", data);
+                let settings = await saveSettingsFields(data);
+                return transformData(settings);
+            },
+            onSuccess: async (data) => {
+                queryClient.setQueryData(["settings_fields"], (oldData) => {
+                    return data ? [...data] : [];
+                });
+                queryClient.invalidateQueries(["settings_fields"]);
+            },
+        });
+
+    return {
+        settings: query.data,
+        saveSettings,
+        getValue,
+        setValue,
+        isSavingSettings: query.isLoading || query.fetchStatus === "fetching",
+        invalidateSettings: () =>
+            queryClient.invalidateQueries(["settings_fields"]),
+    };
 };
 
 export default useSettingsData;
