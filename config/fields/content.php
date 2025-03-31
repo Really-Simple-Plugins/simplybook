90618ea141ad937e92f60321e5c291495fd89514
willembraam <willem.braam@team.blue>
2025-03-31 14:40:25 +0200

    feat: added content settings under general
    For NL14RSP2-47

---
+++ config/fields/content.php
@@ -0,0 +1,52 @@
+<?php
+defined( 'ABSPATH' ) or die( );
+
+return
+    [
+        'booking_page' => [
+            'id'       => 'booking_page',
+            'menu_id'  => 'general',
+            'group_id' => 'content',
+            'type'     => 'select',
+            'label'    => __('Booking page', 'simplybook'),
+            'default'  => true,
+        ],
+        'calendar_page' => [
+            'id'       => 'calendar_page',
+            'menu_id'  => 'general',
+            'group_id' => 'content',
+            'type'     => 'select',
+            'label'    => __('Calendar page', 'simplybook'),
+            'default'  => true,
+        ],
+        'calendar_shortcode' => [
+            'id'       => 'calendar_shortcode',
+            'menu_id'  => 'general',
+            'group_id' => 'content',
+            'type'     => 'text',
+            'disabled' => true,
+            'clickToSelect' => true,
+            'label'    => __('Calendar shortcode', 'simplybook'),
+            'default'  => '[simplybook_widget]',
+        ],
+        'reviews_shortcode' => [
+            'id'       => 'reviews_shortcode',
+            'menu_id'  => 'general',
+            'group_id' => 'content',
+            'type'     => 'text',
+            'disabled' => true,
+            'clickToSelect' => true,
+            'label'    => __('Reviews shortcode', 'simplybook'),
+            'default'  => '[simplybook_reviews]',
+        ],
+        'simplybook_booking_button' => [
+            'id'       => 'simplybook_booking_button',
+            'menu_id'  => 'general',
+            'group_id' => 'content',
+            'type'     => 'text',
+            'disabled' => true,
+            'clickToSelect' => true,
+            'label'    => __('Booking button shortcode', 'simplybook'),
+            'default'  => '[simplybook_booking_button]',
+        ],
+    ];
