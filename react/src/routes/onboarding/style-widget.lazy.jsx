d1bd13f05cc2b28c9e52c083f7d28edd15e28a10
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-02 16:28:20 +0200

    Feature: Changed the format of the style-widget route

--- react/src/routes/onboarding/style-widget.lazy.jsx
+++ react/src/routes/onboarding/style-widget.lazy.jsx
@@ -2,6 +2,8 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 import { __ } from "@wordpress/i18n";
 import OnboardingStep from "../../components/Onboarding/OnboardingStep";
 import Calendar from "../../components/Common/Calendar";
+import LeftColumn from "../../components/Grid/LeftColumn";
+import RightColumn from "../../components/Grid/RightColumn";
 
 const path = "/onboarding/style-widget";
 export const Route = createLazyFileRoute(path)({
@@ -9,16 +11,31 @@ export const Route = createLazyFileRoute(path)({
     component: () => {
         return (
             <>
+            <LeftColumn
+                className={"col-span-6"}
+            >
+                <div className={"text-center"}>
+                    <h2 className={"mt-2 text-lg font-light text-black"}>
+                    {__("Style your calendar", "simplybook")}
+                    </h2>
+                    <h1 className={"text-3xl font-semibold text-black mb-4"}>
+                    {__("What's your style?", "simplybook")}
+                    </h1>
+                </div>  
                 <OnboardingStep
                     path={path}
-                    title={__("What's your style?", "simplybook")}
-                    buttonLabel={__("Next Step: Finish", "simplybook")}
-                    rightColumn={
-                        <div className="h-full">
-                            <Calendar />
-                        </div>
-                    }
+                    primaryButton={{
+                        label: __("Next: Finish Registration", "simplybook"),
+                    }}
                 />
+            </LeftColumn>
+            <RightColumn
+                className={"col-span-5"}
+            >
+            <div className="h-full">
+                <Calendar />
+            </div>
+            </RightColumn>
             </>
         );
     },
