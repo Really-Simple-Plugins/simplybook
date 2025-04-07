507655ea4f22c0a2ebe01fb33ee4622526e8e3e4
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-07 10:16:42 +0200

    Fix: Changed the expireText and added conditionals to render the labels

--- react/src/components/Common/Header.jsx
+++ react/src/components/Common/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
     }, [onboardingCompleted]);
 
     const linkClassName = "text-base p-6 text-tertiary border-b-4  border-transparent [&.active]:border-tertiary focus:outline-hidden";
-    const expireText = `${__("Free trial -", "simplybook")} ${expiresIn} ${__("days left", "simplybook")}`;
+    const expireText = `${subscriptionPlan} - ${expiresIn} ${__("days left", "simplybook")}`;
         
     return (
         <div className="bg-white ">
@@ -64,25 +64,25 @@ const Header = () => {
                     {__("Help Center", "simplybook")}
                 </ButtonLink>
                 <div className="float-right ml-auto flex items-center gap-6 px-4">
-                    {!isExpired && (
+                    {!isExpired && expiresIn && subscriptionPlan && (
                     <Label
                         labelVariant="trial"
                     >
-                       {subscriptionPlan} {expireText}
+                        {expireText}
                     </Label>
                     )}
-                    {isExpired && (
+                    {!isExpired && subscriptionPlan && (
                     <Label
                     labelVariant="trial-expired"
                      >
-                        {subscriptionPlan} {__("Expired", "simplybook")}
+                        {subscriptionPlan} {__("is expired.", "simplybook")}
                     </Label>
                     )}
                     <ButtonLink
                         className="border-primary text-primary hover:border-primary-hover hover:text-primary-hover"
                         btnVariant="ghost"
                         target="_blank"
-                        link="https://rsp9733486412.secure.wp.simplybook.ovh/v2/r/payment-widget#/" 
+                        loginLink="v2/r/payment-widget#/" 
                     >    
                         {__("Upgrade", "simplybook")}
                     </ButtonLink>
