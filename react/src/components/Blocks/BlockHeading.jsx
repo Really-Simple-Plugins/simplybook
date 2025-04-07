cbcc857a78419a73dd1f01a2b6df5f800e3751df
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-07 13:36:59 +0200

    Style: Adjusted padding of the BlockHeading component

--- react/src/components/Blocks/BlockHeading.jsx
+++ react/src/components/Blocks/BlockHeading.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 const BlockHeading = ({ title, controls, className = "" }) => {
   return (
-    <div className={clsx(className, "flex items-center justify-between px-5 py-4")}>
+    <div className={clsx(className, "flex items-center justify-between px-5 py-1")}>
       <h2 className="text-base font-bold">{title}</h2>
       {controls}
     </div>
