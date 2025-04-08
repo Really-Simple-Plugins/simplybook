e414cfb6f5c16de5a1d979b5580616863b119c33
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-08 17:36:49 +0200

    Feature: New props added to the ButtonLink  and new styles

--- react/src/components/Buttons/ButtonLink.tsx
+++ react/src/components/Buttons/ButtonLink.tsx
@@ -17,6 +17,9 @@ const ButtonLink: React.FC<ButtonLinkProps> = ({
     icon = false,
     iconName,
     iconSize,
+    iconClass,
+    iconStyle,
+    reverseIcon = false,
 }) => {
   const { fetchLinkData } = useLoginData();
 
@@ -47,12 +50,13 @@ const ButtonLink: React.FC<ButtonLinkProps> = ({
 
   let buttonVariants = clsx(
     // Base styles
-    "rounded-full transition-all duration-200 px-4 py-2 cursor-pointer",
+    "flex items-center justify-center rounded-full transition-all duration-200 px-4 py-2 cursor-pointer",
     {
       'bg-secondary text-white hover:bg-secondary-dark ' : btnVariant == 'primary',
       'border-black border-3 bg-transparent text-primary hover:border-black hover:text-black ml-4' : btnVariant == 'ghost',
       'bg-tertiary text-white hover:bg-tertiary-dark ' : btnVariant == 'secondary',
       'border-2 border-tertiary bg-transparent text-black hover:bg-tertiary-light ': btnVariant == 'tertiary',
+      'border-3 border-primary text-primary rounded-md py-2 px-6 text-base': btnVariant == 'square',
       'rounded-md py-2 px-6 text-white text-sm': btnVariant == 'square-small',
       'opacity-50 cursor-not-allowed ': btnVariant == 'disabled'
     }
@@ -67,7 +71,7 @@ const ButtonLink: React.FC<ButtonLinkProps> = ({
     <Link to={link} onClick={loginLink ? (e) => loginTo(e, loginLink) : undefined} target={target} className="text-base text-tertiary font-semibold">
       <div className={clsx(buttonVariants, className)}>
           {icon &&
-            <Icon className="mr-2" name={iconName} size={iconSize} />
+            <Icon className={clsx(iconClass, { 'mr-2': !reverseIcon, 'ml-2': reverseIcon })} name={iconName} size={iconSize} style={iconStyle} />
           }
               {children}
       </div>
