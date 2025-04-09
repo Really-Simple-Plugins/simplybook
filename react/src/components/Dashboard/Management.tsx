0a7d60527016ea7109973175e75fd0fc9588f336
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-09 09:30:16 +0200

    Style: Added responsive styling to neighbouring blocks on the dashboard

--- react/src/components/Dashboard/Management.tsx
+++ react/src/components/Dashboard/Management.tsx
@@ -57,7 +57,7 @@ const DataList = [
 
 const Management = () => {
   return (
-    <Block className={"col-span-3 row-span-2"}>
+    <Block className={"col-span-12 sm:col-span-6 2xl:col-span-3 2xl:row-span-2 xl:col-span-4"}>
       <BlockHeading
         title={__("Management", "simplybook")}
         controls={undefined}
