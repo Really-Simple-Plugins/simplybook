7342146f64dffa2f5280b2fb0ae63fdfb6c3b23d
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-10 14:48:50 +0200

    Style: Adjusted the CheckboxInput styling and conditionals

--- react/src/components/Inputs/CheckboxInput.tsx
+++ react/src/components/Inputs/CheckboxInput.tsx
@@ -1,4 +1,5 @@
 import React, {forwardRef, InputHTMLAttributes, useEffect, useState} from "react";
+import clsx from "clsx";
 
 interface CheckboxInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -11,12 +12,18 @@ interface CheckboxInputProps extends InputHTMLAttributes<HTMLInputElement> {
  * @returns {JSX.Element} The rendered input element
  */
 const CheckboxInput = forwardRef<HTMLInputElement, CheckboxInputProps>(
-    ({ label, className, checked, value, onChange, ...props }, ref) => {
+    ({ 
+        label, 
+        className, 
+        checked, 
+        value, 
+        onChange, 
+        ...props 
+    }, ref) => {
 
-        // Change the absolute position depending if there's text next to the checkbox
-        // Do we accept no text next to a checkbox?
-        // Change it later to Tailwindcss v4 syntax
-        let top = !label || label.length === 0 ? "0.5" : "1";
+        const checkBoxClasses = clsx(
+            "w-10 h-6 bg-gray-200  peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:bg-blue-600 peer-checked:after:translate-x-4 peer-checked:after:border-white after:content-[''] after:absolute after:top-1 after:left-0.5 after:bg-white after:border-gray-200 after:border after:rounded-full after:aspect-square after:h-4 after:w-4 after:transition-all" 
+        );
 
         return (
             <label className="relative inline-flex items-center cursor-pointer">
@@ -27,12 +34,12 @@ const CheckboxInput = forwardRef<HTMLInputElement, CheckboxInputProps>(
                     className="sr-only peer"
                     {...props}
                 />
-                <div
-                    className={"w-10 h-6 bg-gray-200  peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:bg-blue-600 peer-checked:after:translate-x-4 peer-checked:after:border-white after:content-[''] after:absolute after:top-"+top+" after:left-0.5 after:bg-white after:border-gray-200 after:border after:rounded-full after:aspect-square after:h-4 after:w-4 after:transition-all"}
-                ></div>
-                <span className={`ml-2 leading-5 font-medium text-black text-label ${className || ""}`}>
-                    {label}
-              </span>
+                <div className={clsx(checkBoxClasses, className)}></div>
+                {label && (                    
+                    <span className={`ml-2 leading-5 font-medium text-black text-label ${className || ""}`}>
+                        {label}
+                    </span>
+                )}
             </label>
         );
     }
