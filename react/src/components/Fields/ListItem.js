d716457c6b18175a5af913e606ac721c67e484a0
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-15 16:38:11 +0200

    Style: Changed the provider/services ListItem to match with the design

--- react/src/components/Fields/ListItem.js
+++ react/src/components/Fields/ListItem.js
@@ -36,32 +36,37 @@ const ListItem = forwardRef(
                     <div className={clsx(upgrade ? "justify-start" : "justify-between", "flex flex-row items-center w-full space-x-3")} >
                         <div className={clsx("flex items-center")}>
                             {domainFetched && !domainHasError && hasPicture &&
-                                <img className="w-15 h-15 max-w-[40px] max-h-[40px]" src={domain + item.picture_preview}  alt={__('Loading', 'simplybook')}/>
+                                <img className="w-20 h-20 max-w-[48px] max-h-[48px] bg-blue-100 text-xs flex items-center justify-center overflow-hidden rounded-md" src={domain + item.picture_preview}  alt={__('Loading', 'simplybook')}/>
                             }
-                            {(!domainFetched || domainHasError || !hasPicture) &&
-                                <div className="min-w-[40px] min-h-[40px] flex items-center justify-center">
-                                    <Icon name="cart"/>
-                                    <div className="font-bold ml-2">
-                                        {item.name}
-                                    </div>
-                                </div>
-                            }
+                            <div className="w-20 h-20 max-w-[48px] max-h-[48px] bg-blue-100 text-xs flex items-center justify-center overflow-hidden rounded-md font-bold">
+                                {item.name.charAt(0).toUpperCase()}
+                            </div>
+                            <div className="font-bold ml-4">
+                                {item.name}
+                            </div>
                         </div>
-                        {domainFetched && !domainHasError &&
+                        {!upgrade && domainFetched && !domainHasError &&
                             <LoginLink
+                                icon={upgrade ? false : true}
+                                iconName="square-arrow-up-right"
+                                iconClass="px-2"
                                 className={"text-black flex items-center"} 
                                 page={link}
                             >
                                 {fullLabel}
                             </LoginLink>
                         }
+                        {upgrade &&(
+                            <p>
+                                {fullLabel}
+                            </p>
+                        )}
                     </div>
                     <div className="flex items-center flex-grow">
                         <div className="relative">
                             {upgrade && <>
                                 <ButtonLink
-                                    className={"bg-tertiary text-white"}
-                                    btnVariant={"square-small"}
+                                    btnVariant={"square-small-tertiary"}
                                     target="_blank"
                                     loginLink="v2/r/payment-widget"
                                 >    
