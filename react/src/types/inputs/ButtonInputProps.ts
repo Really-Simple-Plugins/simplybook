268fa58a17c509705a295c342815de1135f74336
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-15 14:15:42 +0200

    Fix: Added premium to ButtonInput type

--- react/src/types/inputs/ButtonInputProps.ts
+++ react/src/types/inputs/ButtonInputProps.ts
@@ -5,7 +5,7 @@ export type ButtonInputProps = {
     to: string;
     from?: string;
   };
-  btnVariant?: "primary" | "secondary" | "tertiary";
+  btnVariant?: "primary" | "secondary" | "tertiary" | "premium";
   disabled?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement> &
   React.AnchorHTMLAttributes<HTMLAnchorElement>;
