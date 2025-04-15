d7e3145e0a966a472477eb93fc9fd43af2df6248
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-15 16:51:35 +0200

    Style: Adjusted the font-size and deleted a conditional

--- react/src/components/Fields/ListItem.js
+++ react/src/components/Fields/ListItem.js
@@ -33,7 +33,7 @@ const ListItem = forwardRef(
         return (
             <>
                 <div className="w-full flex items-center justify-between px-4 py-5 bg-gray-100 mb-4">
-                    <div className={clsx(upgrade ? "justify-start" : "justify-between", "flex flex-row items-center w-full space-x-3")} >
+                    <div className={clsx(upgrade ? "justify-start" : "justify-between", "flex flex-row items-center w-full space-x-3 text-base")} >
                         <div className={clsx("flex items-center")}>
                             {domainFetched && !domainHasError && hasPicture &&
                                 <img className="w-20 h-20 max-w-[48px] max-h-[48px] bg-blue-100 text-xs flex items-center justify-center overflow-hidden rounded-md" src={domain + item.picture_preview}  alt={__('Loading', 'simplybook')}/>
@@ -45,9 +45,13 @@ const ListItem = forwardRef(
                                 {item.name}
                             </div>
                         </div>
+                        {upgrade &&(
+                            <p>
+                                {fullLabel}
+                            </p>
+                        )}
                         {!upgrade && domainFetched && !domainHasError &&
                             <LoginLink
-                                icon={upgrade ? false : true}
                                 iconName="square-arrow-up-right"
                                 iconClass="px-2"
                                 className={"text-black flex items-center"} 
@@ -56,11 +60,6 @@ const ListItem = forwardRef(
                                 {fullLabel}
                             </LoginLink>
                         }
-                        {upgrade &&(
-                            <p>
-                                {fullLabel}
-                            </p>
-                        )}
                     </div>
                     <div className="flex items-center flex-grow">
                         <div className="relative">
