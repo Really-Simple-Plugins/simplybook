78f40966dc77b8c9a9b09e34003104357e04d010
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-16 11:52:06 +0200

    Style: Cleaned up the template and realigned right column

--- react/src/routes/onboarding/implementation.lazy.jsx
+++ react/src/routes/onboarding/implementation.lazy.jsx
@@ -32,7 +32,7 @@ export const Route = createLazyFileRoute(path)({
                             {__("Almost there!", "simplybook")}
                         </h2>
                         <h1 className={"text-3xl font-semibold text-black mb-4"}>
-                            {__("Let's implement SimplyBook.me", "simplybook")}
+                            {__("Implement SimplyBook.me", "simplybook")}
                         </h1>
                     </div>                     
                     <OnboardingStep
@@ -46,57 +46,58 @@ export const Route = createLazyFileRoute(path)({
                     />
                 </LeftColumn>
                 <RightColumn className={"items-center flex-col flex-wrap justify-center xl:col-span-4 col-span-12 relative w-full"}>
-                    {getValue('implementation') === "manual" &&
-                        <div className={"my-6 text-center"}>
-                            <h1 className={"text-3xl font-semibold text-black m-0 mb-2"}>{__("Implementation", "simplybook")}</h1>
-                            <h2 className={"text-lg font-light text-black m-0 mb-6"}>
-                                {__("Use the below shortcode in a page to show the widget.", "simplybook")}
-                            </h2>
-                            <TextInput 
-                                className="w-full p-4 mb-8" 
-                                clickToSelect={true} 
-                                disabled={true}
-                                value="[simplybook_widget]"
-                            />
-                            <div className="text-base text-gray-600">
-                                <Icon name="info" color="green" className="mr-2"/>
-                                {__("About using shortcodes", "simplybook")}
-                                &nbsp;<a className="underline" href="https://simplybook.me" target="_blank"
-                                        rel="noreferrer">{__("Read more", "simplybook")}</a>
-                            </div>
-                        </div>
-                    }
-                    {getValue('implementation') !== "manual" &&
-                        <div className={"flex flex-col flex-wrap items-center my-6 text-center"}>
-                            <h1 className={"text-3xl font-semibold text-black m-0 mb-2"}>{__("Implementation", "simplybook")}</h1>
-                            <h2 className={"text-lg font-light text-black m-0 mb-6"}>
-                                {__("SimplyBook.me will generate the following pages automatically", "simplybook")}
-                            </h2>
-                            <div className="w-full flex items-center mb-8">
-                                <TextInput className="p-4 flex-grow" value={calendarPageName}
-                                        onChange={(e) => setCalendarPageName(e.target.value)}/>
-                                {calendarPageNameAvailable &&
-                                    <Icon name="check" color="green" className="ml-2 self-center"/>}
-                                {!calendarPageNameAvailable &&
-                                    <Icon name="times" color="red" className="ml-2 self-center"/>}
-                            </div>
-                            <div className="w-full flex items-center mb-8">
-                                <TextInput className="p-4 flex-grow" value={bookingPageName}
-                                        onChange={(e) => setBookingPageName(e.target.value)}/>
-                                {bookingPageNameAvailable &&
-                                    <Icon name="check" color="green" className="ml-2 self-center"/>}
-                                {!bookingPageNameAvailable &&
-                                    <Icon name="times" color="red" className="ml-2 self-center"/>}
-                            </div>
-                            <div className="text-base text-gray-600">
-                                <Icon name="info" color="green" className="mr-2"/>
-                                {__("Generating pages for SimplyBook.me", "simplybook")}
-                                &nbsp;<a className="underline" href="https://simplybook.me" target="_blank"
-                                        rel="noreferrer">{__("Read more", "simplybook")}</a>
-                            </div>
-                        </div>
-
-                    }              
+                    <div className={"flex flex-col flex-wrap items-center my-10 text-center h-full"}>
+                        {getValue('implementation') === "manual" &&
+                            <>
+                                <h1 className={"text-3xl font-semibold text-black m-0 mb-2"}>{__("Implementation", "simplybook")}</h1>
+                                <h2 className={"text-lg font-light text-black m-0 mb-6"}>
+                                    {__("Use the below shortcode in a page to show the widget.", "simplybook")}
+                                </h2>
+                                <TextInput 
+                                    className="w-full p-4 mb-8" 
+                                    clickToSelect={true} 
+                                    disabled={true}
+                                    value="[simplybook_widget]"
+                                />
+                                <div className="text-base text-gray-600">
+                                    <Icon name="info" color="green" className="mr-2"/>
+                                    {__("About using shortcodes", "simplybook")}
+                                    &nbsp;<a className="underline" href="https://simplybook.me" target="_blank"
+                                            rel="noreferrer">{__("Read more", "simplybook")}</a>
+                                </div>
+                            </>
+                        }
+                        {getValue('implementation') !== "manual" &&
+                            <>                    
+                                <h1 className={"text-3xl font-semibold text-black m-0 mb-2"}>{__("Implementation", "simplybook")}</h1>
+                                <h2 className={"text-lg font-light text-black m-0 mb-6"}>
+                                    {__("SimplyBook.me will generate the following pages automatically", "simplybook")}
+                                </h2>
+                                <div className="w-full flex items-center mb-8">
+                                    <TextInput className="p-4 flex-grow" value={calendarPageName}
+                                            onChange={(e) => setCalendarPageName(e.target.value)}/>
+                                    {calendarPageNameAvailable &&
+                                        <Icon name="check" color="green" className="ml-2 self-center"/>}
+                                    {!calendarPageNameAvailable &&
+                                        <Icon name="times" color="red" className="ml-2 self-center"/>}
+                                </div>
+                                <div className="w-full flex items-center mb-8">
+                                    <TextInput className="p-4 flex-grow" value={bookingPageName}
+                                            onChange={(e) => setBookingPageName(e.target.value)}/>
+                                    {bookingPageNameAvailable &&
+                                        <Icon name="check" color="green" className="ml-2 self-center"/>}
+                                    {!bookingPageNameAvailable &&
+                                        <Icon name="times" color="red" className="ml-2 self-center"/>}
+                                </div>
+                                <div className="text-base text-gray-600">
+                                    <Icon name="info" color="green" className="mr-2"/>
+                                    {__("Generating pages for SimplyBook.me", "simplybook")}
+                                    &nbsp;<a className="underline" href="https://simplybook.me" target="_blank"
+                                            rel="noreferrer">{__("Read more", "simplybook")}</a>
+                                </div>
+                            </>
+                        }   
+                    </div>           
                 </RightColumn>
             </>
         );
