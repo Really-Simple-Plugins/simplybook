25d50a82dcdd2d1836cc9bc2f2e00dd176a68dab
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-16 14:35:31 +0200

    Style:  Adjusted the ImplementationInput component

--- react/src/components/Inputs/ImplementationInput.tsx
+++ react/src/components/Inputs/ImplementationInput.tsx
@@ -34,12 +34,10 @@ const ImplementationInput: React.FC<ImplementationInputProps> = ({
                 <button
                     key={option.value}
                     onClick={(e) => handleOnChange(e, option.value, option )}
-                    className={`relative flex flex-col items-start justify-start p-4 rounded-lg border text-center 
+                    className={`relative flex flex-col items-start justify-start px-4 py-3 rounded-lg border text-center 
             transition duration-300 ease-in-out w-full 
             ${
-                        option.is_premium
-                            ? "bg-gray-200 border-gray-200 text-gray-400 cursor-not-allowed"
-                            : value === option.value
+                            value === option.value
                             ? "bg-primary-lighter border-blue-500 cursor-pointer"
                             : "bg-white hover:bg-primary-lighter border-gray-300 hover:border-blue-500 cursor-pointer"
                     }`}
