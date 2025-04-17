f89fd698ed8da921e3a1a3459ba965d20dc8a0e3
willembraam <willem.braam@team.blue>
2025-04-17 08:25:52 +0200

    style: indent with 4 spaces

--- react/src/components/Inputs/ButtonInput.tsx
+++ react/src/components/Inputs/ButtonInput.tsx
@@ -6,47 +6,47 @@ import { ButtonInputProps } from "../../types/inputs/ButtonInputProps";
  * Styled button component
  */
 const ButtonInput: React.FC<ButtonInputProps> = ({
-  className="",
-  type,
-  children,
-  onClick,
-  btnVariant = "primary",
-  disabled = false,
+     className="",
+     type,
+     children,
+     onClick,
+     btnVariant = "primary",
+     disabled = false,
 }) => {
-  let buttonVariants = clsx(
-    // Base styles
-    "flex items-center justify-center rounded-full transition-all duration-200 px-3 py-1 cursor-pointer",
-    {
-      'bg-primary text-white hover:bg-primary-dark !p-4 text-base' : btnVariant == 'primary',
-      'bg-primary text-white hover:bg-primary-dark ' : btnVariant == 'primary-small',
-      'bg-secondary text-white hover:bg-secondary-dark !p-4 text-base' : btnVariant == 'secondary',
-      'bg-secondary text-white hover:bg-secondary-dark ' : btnVariant == 'secondary-small',
-      'bg-tertiary text-white hover:bg-tertiary-light hover:text-tertiary !p-4 text-base': btnVariant == 'tertiary',
-      'bg-tertiary text-white hover:bg-tertiary-light hover:text-tertiary': btnVariant == 'tertiary-small',
-      'border-2 border-black bg-transparent !p-4 text-base' : btnVariant == 'ghost',
-      'border-2 border-black bg-transparent' : btnVariant == 'ghost-small',
-      'bg-primary text-white rounded-md hover:bg-primary-dark !p-4 text-base': btnVariant == 'square',
-      'rounded-md text-white': btnVariant == 'square-small',
-      'border-2 border-primary text-primary rounded-md !p-4 text-base': btnVariant == 'square-ghost',
-      'border-2 border-primary text-primary rounded-md': btnVariant == 'square-ghost-small',
+    let buttonVariants = clsx(
+        // Base styles
+        "flex items-center justify-center rounded-full transition-all duration-200 px-3 py-1 cursor-pointer",
+        {
+            'bg-primary text-white hover:bg-primary-dark !p-4 text-base' : btnVariant == 'primary',
+            'bg-primary text-white hover:bg-primary-dark ' : btnVariant == 'primary-small',
+            'bg-secondary text-white hover:bg-secondary-dark !p-4 text-base' : btnVariant == 'secondary',
+            'bg-secondary text-white hover:bg-secondary-dark ' : btnVariant == 'secondary-small',
+            'bg-tertiary text-white hover:bg-tertiary-light hover:text-tertiary !p-4 text-base': btnVariant == 'tertiary',
+            'bg-tertiary text-white hover:bg-tertiary-light hover:text-tertiary': btnVariant == 'tertiary-small',
+            'border-2 border-black bg-transparent !p-4 text-base' : btnVariant == 'ghost',
+            'border-2 border-black bg-transparent' : btnVariant == 'ghost-small',
+            'bg-primary text-white rounded-md hover:bg-primary-dark !p-4 text-base': btnVariant == 'square',
+            'rounded-md text-white': btnVariant == 'square-small',
+            'border-2 border-primary text-primary rounded-md !p-4 text-base': btnVariant == 'square-ghost',
+            'border-2 border-primary text-primary rounded-md': btnVariant == 'square-ghost-small',
+        }
+    );
+
+    //if props.className is not empty, replace className with props.className
+    if (className.length>0) {
+        buttonVariants = buttonVariants + ' ' + className;
     }
-  );
 
-  //if props.className is not empty, replace className with props.className
-  if (className.length>0) {
-    buttonVariants = buttonVariants + ' ' + className;
-  }
-
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={buttonVariants}
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={buttonVariants}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
 };
 
 ButtonInput.displayName = 'ButtonInput';
