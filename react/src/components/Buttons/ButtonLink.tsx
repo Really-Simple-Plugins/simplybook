05220b6405ede864d92b163f9dbbde780a1edadd
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-04-24 11:58:31 +0200

    Fix: Changed disabled class state management

--- react/src/components/Buttons/ButtonLink.tsx
+++ react/src/components/Buttons/ButtonLink.tsx
@@ -73,7 +73,7 @@ const ButtonLink: React.FC<ButtonLinkProps> = ({
     }
   );
 
-  const disabledClass = 'opacity-50 cursor-not-allowed';
+  const disabledClass = disabled ? 'opacity-50 cursor-not-allowed' : "";
   const reverseClass = reverseIcon ? 'flex-row-reverse' : 'flex-row';
 
 
@@ -90,7 +90,7 @@ const ButtonLink: React.FC<ButtonLinkProps> = ({
       onClick={loginLink ? (e) => loginTo(e, loginLink) : onClick}
       target={target}
       >
-      <div className={clsx(disabled ? disabledClass + buttonVariants : buttonVariants,  reverseClass, className)}>
+      <div className={clsx(buttonVariants,  reverseClass, disabledClass, className)}>
           {iconName &&
             <Icon className={clsx(iconClass, { 'mr-2': !reverseIcon, 'ml-2': reverseIcon })} name={iconName} size={iconSize} style={iconStyle} />
           }
