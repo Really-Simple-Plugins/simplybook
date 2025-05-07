dbeef4edd98c8820a43c8b89293f4a8bb7fcc6a0
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-05-07 13:18:49 +0200

    Feature: After the confirm-email step, we want to hide the sign-in as the account is made

--- react/src/components/Onboarding/OnboardingHeader.jsx
+++ react/src/components/Onboarding/OnboardingHeader.jsx
@@ -3,12 +3,18 @@ import clsx from "clsx";
 import {__} from "@wordpress/i18n";
 import { ReactComponent as Logo } from "../../../../assets/img/logo.svg";
 import SignInModal from "../Modals/SignInModal";
+import useOnboardingData from "../../hooks/useOnboardingData";
+import { useLocation } from "@tanstack/react-router";
 
 const OnboardingHeader = ({
     className, 
     signInLink 
 }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const { getCurrentStepId } = useOnboardingData();
+    const location = useLocation()
+    const currentStepId = getCurrentStepId(location.pathname);
     
     /**
      * Toggle the SignInModal
@@ -25,14 +31,16 @@ const OnboardingHeader = ({
             {isModalOpen && <SignInModal onClose={toggleModal}/>}
             <div className="flex flex-row justify-between w-full px-4">
                 <Logo className="w-40 py-8" />
-                <div className="flex items-center text-base">
-                    <span className={"m-5 text-black"}>
-                        {__("Already got an account?", "simplybook")}
-                    </span>
-                    <a className="font-bold text-primary" href="#" onClick={toggleModal}>
-                        {__("Sign in here")}
-                    </a>
-                </div>
+                {currentStepId < 4 && (                    
+                    <div className="flex items-center text-base">
+                        <span className={"m-5 text-black"}>
+                            {__("Already got an account?", "simplybook")}
+                        </span>
+                        <a className="font-bold text-primary" href="#" onClick={toggleModal}>
+                            {__("Sign in here")}
+                        </a>
+                    </div>
+                )}
             </div>
         </header>
     )
