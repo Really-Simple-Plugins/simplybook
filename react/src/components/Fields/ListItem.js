3e89c1523d8e83bdea17a4dd673c755c45120ce8
willembraam <willem.braam@team.blue>
2025-05-08 08:29:44 +0200

    style: cleanup unused code

--- react/src/components/Fields/ListItem.js
+++ react/src/components/Fields/ListItem.js
@@ -1,31 +1,19 @@
-import {forwardRef, useEffect, useState} from "react";
-import CheckboxInput from "../Inputs/CheckboxInput";
+import {forwardRef, useState} from "react";
 import clsx from "clsx";
 import {__, sprintf } from "@wordpress/i18n";
-import Icon from "../Common/Icon";
 import LoginLink from "../Common/LoginLink";
 import useDomainData from "../../hooks/useDomainData";
 import ButtonLink from "../Buttons/ButtonLink";
-/**
- * HiddenField component
- * @param {string} id
- * @param {string} name
 
- */
 const ListItem = forwardRef(
-    ({ 
-        upgrade, 
-        link, 
-        item, 
-        label, 
-        ...props 
+    ({
+        upgrade,
+        link,
+        item,
+        label,
+        ...props
     }, ref) => {
 
-        const [visible, setVisible] = useState(!!item?.is_visible || false);
-        const onChange = (e) => {
-            console.log('onChange', e.target.checked, "for id ", item.id);
-            setVisible(e.target.checked);
-        };
         const { domain, domainFetched, hasError: domainHasError } = useDomainData();
         const hasPicture = domainFetched && item.picture_preview && item.picture_preview.length > 0;
         const fullLabel = upgrade ? ' |  '+sprintf(__("Get unlimited %s", "simplybook"), label.toLowerCase()) : __("Edit", "simplybook");
@@ -38,7 +26,7 @@ const ListItem = forwardRef(
                             {domainFetched && !domainHasError && hasPicture &&
                                 <img className="w-20 h-20 max-w-[48px] max-h-[48px] bg-blue-100 text-xs flex items-center justify-center overflow-hidden rounded-md" src={domain + item.picture_preview}  alt={__('Loading', 'simplybook')}/>
                             }
-                            {domainFetched && !domainHasError && !hasPicture &&                            
+                            {domainFetched && !domainHasError && !hasPicture &&
                                 <div className="w-20 h-20 max-w-[48px] max-h-[48px] bg-blue-100 text-xs flex items-center justify-center overflow-hidden rounded-md font-bold">
                                     {item.name.charAt(0).toUpperCase()}
                                 </div>
@@ -57,7 +45,7 @@ const ListItem = forwardRef(
                                 icon={true}
                                 iconName="square-arrow-up-right"
                                 iconClass="px-2"
-                                className={"text-black flex items-center"} 
+                                className={"text-black flex items-center"}
                                 page={link}
                             >
                                 {fullLabel}
@@ -72,7 +60,7 @@ const ListItem = forwardRef(
                                     btnVariant={"square-small"}
                                     target="_blank"
                                     loginLink="v2/r/payment-widget"
-                                >    
+                                >
                                     {__("Upgrade", "simplybook")}
                                 </ButtonLink>
                             </>}
