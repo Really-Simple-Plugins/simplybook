2e9e00b5c44541eafd4af546ab1f35935cbd36cc
willembraam <willem.braam@team.blue>
2025-05-09 15:04:02 +0200

    fix: render demo widget when not logged in with alert above it
    Just like it was with the legacy plugin

--- app/traits/HasViews.php
+++ app/traits/HasViews.php
@@ -5,19 +5,28 @@ use SimplyBook\App;
 trait HasViews
 {
     /**
-     * Method for returning the desired view
+     * Method for returning the desired view as a string
      */
-    public function render(string $path, array $variables = [], string $extension = 'php'): void
+    public function view(string $path, array $variables = [], string $extension = 'php'): string
     {
         $filePath = App::env('plugin.view_path') . $path . '.' . $extension;
 
-        if (empty($filePath)) echo '';
+        if (empty($filePath)) return '';
 
         extract($variables);
 
         ob_start();
         require $filePath;
-        echo ob_get_clean();
+        return ob_get_clean();
+    }
+
+
+    /**
+     * Method for outputting the desired view
+     */
+    public function render(string $path, array $variables = [], string $extension = 'php'): void
+    {
+        echo $this->view($path, $variables, $extension);
     }
 
 }
