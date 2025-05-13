52e0ac60cd7ef623fc8fa7f95d23cfd4d3d949ec
willembraam <willem.braam@team.blue>
2025-05-13 08:45:32 +0200

    fix: ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r too
    We do want to use this in production as its only used in WP_DEBUG is disabled

--- app/traits/LegacyHelper.php
+++ app/traits/LegacyHelper.php
@@ -167,14 +167,13 @@ trait LegacyHelper {
      * Log a message if WP_DEBUG is enabled
      *
      * @param string | object | array $message
-     *
      * @return void
      */
     public function log(  $message ): void {
         if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 			$prepend = 'SimplyBook.me: ';
             if ( is_array( $message ) || is_object( $message ) ) {
-                /* phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log */
+                /* phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log, WordPress.PHP.DevelopmentFunctions.error_log_print_r */
 				error_log( $prepend . print_r( $message, true ) );
 			} else {
                 /* phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log */
