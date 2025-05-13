3c96f6323d1996c4d5aec345aee83aa664ab460c
willembraam <willem.braam@team.blue>
2025-05-13 10:08:45 +0200

    fix: Given paths outside of the viewpath can no longer be rendered to prevent anything malicious
    Because of that we can ignore the phpcs error as we have full control of the output

--- app/traits/HasViews.php
+++ app/traits/HasViews.php
@@ -6,12 +6,19 @@ trait HasViews
 {
     /**
      * Method for returning the desired view as a string
+     * @throws \LogicException
      */
     public function view(string $path, array $variables = [], string $extension = 'php'): string
     {
-        $filePath = App::env('plugin.view_path') . $path . '.' . $extension;
+        $basePath = App::env('plugin.view_path');
+        $filePath = realpath($basePath . $path . '.' . $extension);
 
-        if (empty($filePath)) return '';
+        // Someone is doing something dirty
+        if (!$filePath || strpos($filePath, $basePath) !== 0) {
+            throw new \LogicException('Given path is not valid: ' . esc_html($filePath));
+        }
+
+        if (empty($filePath) || !file_exists($filePath) || !is_readable($filePath)) return '';
 
         extract($variables);
 
@@ -20,17 +27,16 @@ trait HasViews
         return ob_get_clean();
     }
 
-
     /**
-     * Method for outputting the desired view
+     * Method for outputting the desired view.
+     * @internal we can ignore the phpcs error because we validate in {@see view}
+     * that the executed path is in our plugin. And we escape all variables
+     * in our views, so we have full control.
      */
     public function render(string $path, array $variables = [], string $extension = 'php'): void
     {
-        echo $this->view(
-            esc_html($path),
-            $variables,
-            esc_html($extension)
-        );
+        // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+        echo $this->view($path, $variables, $extension);
     }
 
 }
