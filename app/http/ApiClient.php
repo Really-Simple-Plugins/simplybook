b5b2618047e898af8d9b3abd4c7e3a827e925934
willembraam <willem.braam@team.blue>
2025-05-13 08:20:45 +0200

    fix: use esc_sql for dynamic get_option name

--- app/http/ApiClient.php
+++ app/http/ApiClient.php
@@ -251,7 +251,7 @@ class ApiClient
         if ( $refresh ) {
             $type = $type . '_refresh';
         }
-        $token = get_option("simplybook_token_$type", '');
+        $token = get_option("simplybook_token_" . esc_sql($type), '');
 
         return $this->decrypt_string($token);
     }
