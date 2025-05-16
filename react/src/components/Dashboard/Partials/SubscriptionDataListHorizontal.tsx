2d5a96a90634cbf7578880d37a63cd49cfc73338
willembraam <willem.braam@team.blue>
2025-05-16 13:11:54 +0200

    fix: in Progress.tsx we only show "trial" plan info
    The SubscriptionDataListHorizontal.tsx now accept "target", if that does not match the subscription plan then the subscription data is not rendered.

--- react/src/components/Dashboard/Partials/SubscriptionDataListHorizontal.tsx
+++ react/src/components/Dashboard/Partials/SubscriptionDataListHorizontal.tsx
@@ -7,36 +7,38 @@ import useSubscriptionData from "../../../hooks/useSubscriptionData";
 
 const SubscriptionDataListHorizontal: React.FC<SubscriptionDataListHorizontalProps> = ({
     className,
+    target,
 }) => {
     // Load the subscription data
     const {
+        subscriptionPlan,
         expiresIn,
         isExpired,
         isLoading,
         hasError
     } = useSubscriptionData();
 
+    // If a target is given we abort when the subscription plan does not match
+    if (target && subscriptionPlan.toUpperCase() !== target.toUpperCase()) {
+        return null;
+    }
+
+    let message = subscriptionPlan + ': ' + expiresIn + ' ' + __("days left", "simplybook");
+    let expiredMessage = subscriptionPlan + ' ' + __("is expired", "simplybook");
+
     return (
         <>
-        {!isLoading && !hasError && (
-            <ul className={clsx("list-none flex flex-col  2xl:flex-row 2xl:justify-end 2xl:flex-wrap gap-2", className)}>
-                <ListWithIcon
-                    iconColor={(expiresIn == 0 || isExpired) ? "red" : "var(--color-green-600)"}
-                    iconName={isExpired ? "circle-xmark" : "circle-check"}
-                    iconSize="md"
-                >
-                    {isExpired ? (
-                        <>
-                            {__("Your trial has expired", "simplybook")}
-                        </>
-                    ) : (
-                        <>
-                            {__('Trial:', 'simplybook')} {expiresIn} {__("days left", "simplybook")}
-                        </>
-                    )}
-                </ListWithIcon>
-            </ul>
-        )}
+            {!isLoading && !hasError && (
+                <ul className={clsx("list-none flex flex-col  2xl:flex-row 2xl:justify-end 2xl:flex-wrap gap-2", className)}>
+                    <ListWithIcon
+                        iconColor={(expiresIn == 0 || isExpired) ? "red" : "var(--color-green-600)"}
+                        iconName={isExpired ? "circle-xmark" : "circle-check"}
+                        iconSize="md"
+                    >
+                        {isExpired ? expiredMessage : message}
+                    </ListWithIcon>
+                </ul>
+            )}
         </>
     );
 };
