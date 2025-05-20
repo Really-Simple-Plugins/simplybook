f24a849e3117537aa36cd9aebc55a30ae3db9a9f
Jeroen van der Kooij <jeroen.vanderkooij@team.blue>
2025-05-20 09:18:25 +0200

    Style: Settings container is now matched with dashboard container

--- react/src/routes/settings.lazy.jsx
+++ react/src/routes/settings.lazy.jsx
@@ -1,10 +1,12 @@
 import { createLazyFileRoute, Outlet } from "@tanstack/react-router";
+import clsx from "clsx";
 import Header from "../components/Common/Header";
 import ErrorBoundary from "../components/Common/ErrorBoundary";
 import SettingsMenu from "../components/Settings/SettingsMenu";
 import NotificationSidebar from "../components/Settings/Partials/NotificationSidebar";
 import NotificationsBox from "../components/Dashboard/Partials/NotificationsBox";
 import {NotificationProvider} from "../context/NotificationContext";
+
 
 export const Route = createLazyFileRoute("/settings")({
     component: () => <Settings />,
@@ -15,7 +17,10 @@ const Settings = () => {
         <NotificationProvider>
             <Header />
             <div className="mx-auto flex max-w-screen-2xl w-full">
-                <div className="m-5 w-full grid grid-cols-12 gap-y-4 lg:gap-y-4 gap-x-4 px-4">
+                <div className={clsx(
+                    "my-4 w-full grid grid-cols-12 gap-y-4 gap-x-4",
+                    "lg:gap-y-4"
+                )}>
                         <SettingsMenu />
                         <ErrorBoundary>
                             <Outlet />
