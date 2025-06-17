eb3f8dd3db8eee3062a5525fe1a9f606c33bac17
Mirco Baalmans <mirco.baalmans@team.blue>
2025-06-17 15:20:35 +0200

    fix: new build of widget block to include changes made for new default provider value

--- assets/block/build/index.asset.php
+++ assets/block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5ccbc3faee2c1bfe2f87');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '336367f4cf6c243e9aaf');
