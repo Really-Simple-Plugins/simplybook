80edc86d6aa3dab0fabc5a2fa8774597e14b8a42
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-06-27 18:01:42 +0200

    fix: adjusted styling of the TaskModal

--- react/src/components/Modals/TaskModal.tsx
+++ react/src/components/Modals/TaskModal.tsx
@@ -12,24 +12,23 @@ type TaskModalProps = {
 const TaskModal: React.FC<TaskModalProps> = ({ isModalOpen, taskModalContent, onModalClose }) => {
 
     return (
-        <Modal isOpen={isModalOpen} closeButton={false} onClose={onModalClose}>
-            <div style={{...( taskModalContent?.backgroundImage && {backgroundImage: `url(${taskModalContent?.backgroundImage})`})}}
-                className="flex flex-row gap-4">
+        <Modal isOpen={isModalOpen} closeButton={true} onClose={onModalClose}>
+            <div className="flex flex-row gap-4 mb-3">
                 <div className={clsx(
-                    taskModalContent?.image ? "sm:max-w-2/3" : "",
+                    taskModalContent?.image ? "sm:max-w-1/2" : "",
                     "flex flex-col gap-3")}>
                     {taskModalContent?.title && (
-                        <h2>{taskModalContent?.title}</h2>)}
+                        <h2 className="text-xl font-bold text-tertiary">{taskModalContent?.title}</h2>)}
                     {taskModalContent?.content?.sections && (
                         taskModalContent?.content?.sections.map((section) => (
-                            <p>{section}</p>
+                            <p className="m-0 text-justify sm:text-left">{section}</p>
                         ))
                     )}
                     {taskModalContent?.buttons && (
-                        <div className="flex flex-col sm:flex-row gap-4">
+                        <div className="flex flex-row gap-4">
                             {taskModalContent?.buttons.map((button) => (
-                                <div className="flex flex-col gap-3">
-                                    <img src={button.qr} alt=""/>
+                                <div className="flex flex-col gap-3 justify-center items-center">
+                                    <img width="100%" src={button.qr} alt=""/>
                                     <a href={button.link} target="_blank">
                                         <img src={button.image} width="200px" alt=""/>
                                     </a>
@@ -37,16 +36,22 @@ const TaskModal: React.FC<TaskModalProps> = ({ isModalOpen, taskModalContent, on
                             ))}
                         </div>)}
                     {taskModalContent?.footer && (
-                        <span>{taskModalContent.footer.text}
-                            <a href={taskModalContent.footer.link}
-                               target="_blank">{taskModalContent.footer.linkText}</a>
-                        </span>)}
+                        <div className="my-2">
+                            <span>{taskModalContent.footer.text}
+                                <a href={taskModalContent.footer.link}
+                                   target="_blank">{taskModalContent.footer.linkText}</a>
+                            </span>
+                        </div>)}
                 </div>
                 {taskModalContent?.image && (
-                    <div className="hidden sm:flex flex-col gap-3">
+                    <div className="hidden sm:flex flex-col gap-3 grow-1">
                         <img className="h-full" src={`${taskModalContent?.image}`} alt="test"/>
                     </div>)}
             </div>
+            {taskModalContent?.backgroundImage && (
+                <div className="modal-background absolute inset-0 -z-1"
+                     style={{...(taskModalContent?.backgroundImage && {background: `no-repeat center/150% url(${taskModalContent?.backgroundImage})`})}}></div>
+            )}
         </Modal>
     );
 };
