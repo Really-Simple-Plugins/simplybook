3e9ffec9c593dbf6ae5279e978778ff4efca882c
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-07-03 09:41:28 +0200

    feat: nested array implementation

--- react/src/types/TaskModal.ts
+++ react/src/types/TaskModal.ts
@@ -1,25 +1,22 @@
 interface CallsToAction {
     button?: ModalButton,
-    qr?: string,
 }
 
 interface ModalButton {
     image?: string,
     link?: string,
     text?: string,
+    showText?: boolean,
+}
+
+interface ContentOptions {
+    type?: string,
+    content?: string | CallsToAction[],
+    altText?: string,
 }
 
 export interface TaskModal {
-    image?: string,
     backgroundImage?: string,
-    title?: string,
-    content?: {
-        sections: string[]
-    }
-    callsToAction?: CallsToAction[]
-    footer?: {
-        text: string,
-        link: string,
-        linkText: string,
-    }
+    callsToAction?: CallsToAction[],
+    columns?: ContentOptions[][],
 }
