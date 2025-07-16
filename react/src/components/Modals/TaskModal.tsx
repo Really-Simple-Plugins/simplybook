4c837c9f0823c92e415234cc6d3dda34b1681fba
Renee van Velthuijsen <renee.vanvelthuijsen@team.blue>
2025-07-16 08:46:13 +0200

    feat: implemented the new taskModal
    -including new generated styling

--- react/src/components/Modals/TaskModal.tsx
+++ react/src/components/Modals/TaskModal.tsx
@@ -1,73 +1,36 @@
 import React from "react";
 import Modal from '../Common/Modal';
-import { TaskModal as TaskModalType } from "../../types/TaskModal";
-import clsx from "clsx";
+import InstallAppTaskModalContent from "./Tasks/InstallAppTaskModalContent"
 
 type TaskModalProps = {
     isModalOpen: boolean,
-    // taskModalContent: TaskModalType | undefined,
-    taskModalContent: any,
-    onModalClose: () => void
+    taskModalId: string,
+    onModalClose: () => void,
 };
 
-const TaskModal: React.FC<TaskModalProps> = ({ isModalOpen, taskModalContent, onModalClose }) => {
-    const cssContentGridTemplateAreas = "'col0-title0 col1-images0'" + "'col0-text0 col1-images0'" + "'col0-text1 col1-images0'" + "'col0-images0 col1-images0'" + "'col0-callsToAction0 col1-images0'";
+const TaskModal: React.FC<TaskModalProps> = ({ isModalOpen, taskModalId, onModalClose }) => {
+    if (!taskModalId) {
+        console.warn("No taskModalId was provided.");
+        return null;
+    }
+
+    let taskModalContent;
+
+    switch (taskModalId) {
+        case "install_app_task":
+            taskModalContent = (<InstallAppTaskModalContent />);
+            break;
+        default:
+            console.error("The taskModalId provided is not valid.");
+    }
+
+    if(!taskModalContent){
+        return null;
+    }
 
     return (
         <Modal isOpen={isModalOpen} closeButton={true} onClose={onModalClose}>
-            {taskModalContent?.header?.title && (
-                <div>
-                    {taskModalContent.header.title}
-                </div>
-            )}
-            {taskModalContent?.content && (
-                <div className="grid gap-y-4 mb-3 grid-cols-1 sm:grid-cols-2" style={{gridTemplateAreas: cssContentGridTemplateAreas, gridTemplateRows: 'auto'}}>
-                    {taskModalContent?.content.map((column: any, columnIndex: number) => (
-                        Object.keys(column).map((contentType) => (
-                            column[contentType].map((item: any, itemIndex: number) => {
-                                const gridAreaName = `col${columnIndex}-${contentType}${itemIndex}`
-                                return (
-                                    <div style={{gridArea: gridAreaName}} className={clsx(
-                                        (contentType === 'images' && (gridAreaName === "col1-images0" ? "hidden sm:flex self-center justify-self-center" : "sm:max-w-[80%] lg:max-w-[70%]")),
-                                        (contentType === 'title' && "flex items-center justify-between"),
-                                        (contentType === 'text' && "text-sm mb-2"),
-                                        (contentType === 'callsToAction' && "flex flex-row gap-3 justify-center items-center w-full sm:max-w-[80%] lg:max-w-[70%]"),
-                                    )}>
-                                        {contentType === 'title' && (
-                                            <h2 className="text-lg font-semibold m-0">{item}</h2>
-                                        )}
-                                        {contentType === 'text' && (
-                                            item
-                                        )}
-                                        {contentType === 'images' && (
-                                            <img src={item.src} alt={item.altText}/>
-                                        )}
-                                        {contentType === 'callsToAction' && (
-                                            item.map((cta: any) => (
-                                                cta.button && (
-                                                    <a href={cta.button.link} target="_blank">
-                                                        {cta.button.image && (
-                                                            <img src={cta.button.image} width="200px" alt={cta.button.text ?? "Link to download the Admin App"}/>
-                                                        )}
-                                                        {(cta.button.text && cta.button.showText) && (
-                                                            <p>{cta.button.text}</p>
-                                                        )}
-                                                    </a>
-                                                )
-                                            ))
-                                        )}
-                                    </div>
-                                )
-                            })
-                        )))
-                    )}
-                </div>
-            )}
-            {taskModalContent?.background && (
-                <div className="modal-background absolute inset-0 -z-1"
-                     style={{...(taskModalContent?.background.image && {background: `no-repeat center/150% url(${taskModalContent?.background.image})`})}}>
-                </div>
-            )}
+            {taskModalContent}
         </Modal>
     );
 };
