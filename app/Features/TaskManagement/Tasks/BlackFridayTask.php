29c8d891919f5da494412cd18cf9b76525daca0f
willembraam <willem.braam@team.blue>
2025-12-12 11:39:32 +0100

    fix: added EnvironmentConfig dependency to fix possible fatal error on deprecated ->env call from App::getInstance()

--- app/Features/TaskManagement/Tasks/BlackFridayTask.php
+++ app/Features/TaskManagement/Tasks/BlackFridayTask.php
@@ -3,6 +3,7 @@
 namespace SimplyBook\Features\TaskManagement\Tasks;
 
 use SimplyBook\Bootstrap\App;
+use SimplyBook\Support\Helpers\Storages\EnvironmentConfig;
 
 class BlackFridayTask extends AbstractTask
 {
@@ -14,13 +15,26 @@ class BlackFridayTask extends AbstractTask
     protected bool $required = true;
 
     /**
-     * We hide this task by default and it is updated to "upgrade" status
+     * @since 3.2.4 bumped version due to the addition of a constructor
+     * argument.
+     */
+    protected string $version = '1.0.1';
+
+    /**
+     * The environment configuration
+     */
+    private EnvironmentConfig $env;
+
+    /**
+     * We hide this task by default, and it is updated to "upgrade" status
      * during Black Friday period ánd only for Trial users in the
      * {@see TaskManagementListener}
      */
-    public function __construct()
+    public function __construct(EnvironmentConfig $env)
     {
         $this->hide();
+
+        $this->env = $env;
     }
 
     /**
@@ -31,8 +45,8 @@ class BlackFridayTask extends AbstractTask
         return sprintf(
             /* translators: 1: discount percentage, 2: promo code */
             __('Black Friday sale! Get %1$s Off SimplyBook.me with code %2$s', 'simplybook'),
-            '<strong>' . App::getInstance()->env->getString('simplybook.black_friday.discount_percentage') . '%</strong>',
-            '<code>' . App::getInstance()->env->getString('simplybook.black_friday.promo_code') . '</code>'
+            '<strong>' . $this->env->getString('simplybook.black_friday.discount_percentage') . '%</strong>',
+            '<code>' . $this->env->getString('simplybook.black_friday.promo_code') . '</code>'
         );
     }
 
