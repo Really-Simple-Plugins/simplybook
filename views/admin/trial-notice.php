604520f8b106faf46bc8a266592af374179e9b86
kelvin.kuiper <kelvin.kuiper@team.blue>
2026-09-16 14:19:38 +0000

    feat: add snooze and dismiss buttons to the trial notice view
    The notice now contains a form with two buttons. "Remind me tomorrow"
    hides the notice for one day. "Don't show again" hides the notice
    for good. The layout follows the review notice.
    
    Closes: NL14RSP2-417 - Written by Devin

--- views/admin/trial-notice.php
+++ views/admin/trial-notice.php
@@ -4,6 +4,8 @@
  * @var string $logoUrl
  * @var string $message
  * @var string $plansPricesUrl
+ * @var string $trialAction
+ * @var string $trialNonceName
  */
 ?>
 
@@ -30,11 +32,30 @@
     }
     .rsp-trial .rsp-buttons-row {
         margin-top:10px;
+        display: flex;
+        align-items: center;
+    }
+    .rsp-trial .rsp-container .dashicons {
+        margin-right:5px;
+        margin-left:15px;
+    }
+    .rsp-trial .rsp-trial-form button.link {
+        background: none;
+        border: none;
+        color: #2271b1;
+        text-decoration: underline;
+        cursor: pointer;
+        padding: 0;
+        font-size: inherit;
     }
     .rsp-trial .rsp-trial-content {
         margin-left: 30px;
     }
     <?php if (is_rtl()) : ?>
+         .rsp-trial .rsp-container .dashicons {
+             margin-left:5px;
+             margin-right:15px;
+         }
          .rsp-trial .rsp-trial-content {
              margin-left:0;
              margin-right:30px;
@@ -49,7 +70,9 @@
 <div id="message" class="notice notice-warning is-dismissible rsp-trial really-simple-plugins" data-notice-type="trial">
     <div class="rsp-container">
         <div class="rsp-trial-image"><img src="<?php echo esc_url($logoUrl); ?>" alt="simplybook-logo"></div>
-        <div class="rsp-trial-content">
+        <form class="rsp-trial-content rsp-trial-form" action="" method="POST">
+            <?php wp_nonce_field($trialAction, $trialNonceName); ?>
+            <input type="hidden" name="rsp_trial_form" value="1">
             <?php echo wp_kses_post(wpautop($message)); ?>
             <div class="rsp-buttons-row">
                 <a
@@ -58,7 +81,15 @@
                 >
                     <?php esc_html_e('Discover plans', 'simplybook'); ?>
                 </a>
+                <div class="dashicons dashicons-calendar"></div>
+                <button type="submit" class="link" name="rsp_trial_choice" value="later">
+                    <?php esc_html_e('Remind me tomorrow', 'simplybook'); ?>
+                </button>
+                <div class="dashicons dashicons-no-alt"></div>
+                <button type="submit" class="link" name="rsp_trial_choice" value="never">
+                    <?php esc_html_e('Don\'t show again', 'simplybook'); ?>
+                </button>
             </div>
-        </div>
+        </form>
     </div>
 </div>
