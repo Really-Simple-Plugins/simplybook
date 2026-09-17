dc360868016e90313c2bc1741e1fd907944c04db
willem.braam <willem.braam@team.blue>
2026-09-17 06:28:05 +0000

    feat: show the trial ended task as urgent
    The listener now sets the status urgent instead of open when the trial has ended.
    
    Closes: NL14RSP2-422 - Written by Devin

--- app/Features/TaskManagement/TaskManagementListener.php
+++ app/Features/TaskManagement/TaskManagementListener.php
@@ -148,7 +148,7 @@ class TaskManagementListener implements ListenerInterface
 
         if (!empty($subscription) && $subscription === 'Trial') {
             if ($isExpired) {
-                $this->service->openTask(
+                $this->service->flagTaskUrgent(
                     Tasks\TrialExpiredTask::IDENTIFIER
                 );
             }
